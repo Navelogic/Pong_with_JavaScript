4261b6711a16b1ad4ddba943437f8ab2d44ec81c
Arian Weslley <weslleysantoszbd@gmail.com>
2023-04-05 22:56:44 -0300

    Update main.js

--- main.js
+++ main.js
@@ -8,15 +8,31 @@ let velocityXCircle = 6; let velocityYCircle = 6;
 function setup() {
   createCanvas(600, 400);
 }
-function draw() {
-  background(0);
+
+function drawingTheCircle(){
   circle(xCircle, yCircle, diameter);
+}
+
+function movingTheCircle(){
   xCircle += velocityXCircle;
   yCircle += velocityYCircle;
+}
 
+function collisionCheck(){
   if(xCircle + raio > width || xCircle - raio < 0){
     velocityXCircle *= -1;
   } else if (yCircle + raio > height || yCircle - raio < 0){
     velocityYCircle *= -1;
   }
+}
+
+
+
+
+// Main
+function draw() {
+  background(0);
+  drawingTheCircle();
+  movingTheCircle();
+  collisionCheck();
 }
