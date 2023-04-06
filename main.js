933280d2cf1ed10e41e89a44c2b2ea061a713fdf
Arian Weslley <weslleysantoszbd@gmail.com>
2023-04-05 22:30:49 -0300

    Adding movement to the circle

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-function setup() {
-  createCanvas(1863, 965);
-}
+// Variables
+xCircle = 300; yCircle = 200; diameter = 15;
 
+velocityXCircle = 6; velocityYCircle = 6;
+
+// Functions
+function setup() {
+  createCanvas(600, 400);
+}
 function draw() {
   background(0);
-  circle(100, 100, 100);
+  circle(xCircle, yCircle, diameter);
+  xCircle += velocityXCircle;
+  yCircle += velocityYCircle;
 }
